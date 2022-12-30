60e53d8c053f3c7b3214bcbeeaa27d23ad2d3812
Noxmor <janek.drews@t-online.de>
2022-12-30 15:19:55 +0100

    Added logging macros

--- hellfire/src/core/log.c
+++ hellfire/src/core/log.c
@@ -8,7 +8,7 @@
 
 #include "core/core.h"
 
-void logger_log(const char* message, ...)
+void logger_log(const char* name, const char* message, ...)
 {
 	__builtin_va_list args = 0;
 	va_start(args, message);
@@ -17,11 +17,9 @@ void logger_log(const char* message, ...)
 	char* formatted_message = malloc(len + 1);
 	vsprintf(formatted_message, message, args);
 	va_end(args);
-	vprintf(formatted_message, args);
 
 	time_t now = time(0);
-	char* time_str = ctime(&now);
-	time_str[strlen(time_str) - 1] = '\0';
+	struct tm* time_info = localtime(&now);
 
-	printf("[%s]: %s", time_str, formatted_message);
+	printf("[%02d:%02d:%02d] %s: %s\n", time_info->tm_hour, time_info->tm_min, time_info->tm_sec, name, formatted_message);
 }
--- hellfire/src/core/log.h
+++ hellfire/src/core/log.h
@@ -1,6 +1,9 @@
 #ifndef HF_LOG_H
 #define HF_LOG_H
 
-void logger_log(const char* message, ...);
+void logger_log(const char* name, const char* message, ...);
+
+#define HF_CORE_LOG(...) logger_log("HELLFIRE", __VA_ARGS__)
+#define HF_LOG(...) logger_log("APPLICATION", __VA_ARGS__)
 
 #endif
