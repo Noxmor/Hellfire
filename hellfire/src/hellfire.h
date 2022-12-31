fc306ff4f9761a4915dd834917c4c50ab34c7f97
Noxmor <janek.drews@t-online.de>
2022-12-31 01:10:22 +0100

    Added Memory System

--- hellfire/src/hellfire.h
+++ hellfire/src/hellfire.h
@@ -3,7 +3,9 @@
 
 #include "core/core.h"
 #include "core/platform_detection.h"
+#include "core/log.h"
 #include "core/entry_point.h"
 #include "core/application.h"
+#include "core/memory_system.h"
 
 #endif
