b0939daf9e9076e3892e58e8ad28c0172127552d
Noxmor <janek.drews@t-online.de>
2022-12-31 01:12:00 +0100

    Now using the Memory System

--- editor/src/main.c
+++ editor/src/main.c
@@ -1,10 +1,8 @@
 #include <hellfire.h>
 
-#include <stdlib.h>
-
 Application* application_create(void)
 {
-	Application* app = malloc(sizeof(Application));
+	Application* app = memory_system_malloc(sizeof(Application), HF_MEMORY_TAG_UNKNOWN);
 	app->width = 1600;
 	app->height = 900;
 	app->title = "Hellfire-Editor";
--- hellfire/src/core/entry_point.h
+++ hellfire/src/core/entry_point.h
@@ -6,6 +6,7 @@
 #include "core/core.h"
 #include "core/application.h"
 #include "core/log.h"
+#include "core/memory_system.h"
 
 extern Application* application_create(void);
 
@@ -18,12 +19,16 @@ int main(void)
 	HF_CORE_ERROR("Error");
 	HF_CORE_FATAL("Fatal");
 	
+	memory_system_init();
+
 	HF_CORE_INFO("Creating application...");
 	Application* app = application_create();
 
 	application_run(app);
 
 	application_shutdown(app);
+
+	memory_system_shutdown();
 
 	return 0;
 }
